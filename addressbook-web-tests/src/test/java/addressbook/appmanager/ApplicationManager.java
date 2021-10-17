0b78de1bb00486ed925ded2eec82dae4732a8129
anbabik <nannnynu@yandex.ru>
2021-10-17 21:18:48 +0300

    Added support to run tests for Firefox, Chrome, IE browsers

--- addressbook-web-tests/src/test/java/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/addressbook/appmanager/ApplicationManager.java
@@ -1,7 +1,10 @@
 package addressbook.appmanager;
 
-import org.openqa.selenium.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
 
 import java.util.concurrent.TimeUnit;
 
@@ -17,9 +20,23 @@ public class ApplicationManager {
     private HelperBase helperBase;
     private String baseUrl;
     private StringBuffer verificationErrors = new StringBuffer();
+    private String browser;
+
+    public ApplicationManager(String browser) {
+        this.browser = browser;
+    }
 
     public void init() {
-        wd = new FirefoxDriver();
+  //      String browser = BrowserType.FIREFOX;
+
+        if (browser==BrowserType.FIREFOX) {
+            wd = new FirefoxDriver();
+        } else if (browser==BrowserType.CHROME) {
+            wd = new ChromeDriver();
+        } else if (browser==BrowserType.IE) {
+            wd = new InternetExplorerDriver();
+        }
+
         baseUrl = "https://www.google.com/";
         wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         wd.get("http://localhost/addressbook/");
